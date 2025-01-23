8133a276c8ec780e0c630dd3f43b9d130327b89d
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-23 11:46:30 +0700

    plot scatter map

--- read_plot_csv1.py
+++ read_plot_csv1.py
@@ -7,3 +7,8 @@ points = genfromtxt('data/points.csv', delimiter=',')           # [x y]
 distances = genfromtxt('data/distances.csv', delimiter=",")     # [distance]
 
 
+plt.scatter(points[:, 0], points[:, 1], c=distances, cmap='winter')
+
+plt.colorbar(label='Distance')
+
+plt.show()
