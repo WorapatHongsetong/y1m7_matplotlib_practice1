f87159527a6cf99260f724a238bde8329e837ccf
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-23 11:46:08 +0700

    convert csv to array

---
+++ read_plot_csv1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import genfromtxt
+
+
+points = genfromtxt('data/points.csv', delimiter=',')           # [x y]
+distances = genfromtxt('data/distances.csv', delimiter=",")     # [distance]
+
+
